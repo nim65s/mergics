a60b5b19a0a9860de5a4d052547bd15db9e7af3b
Guilhem Saurel <guilhem.saurel@laas.fr>
2020-10-06 18:24:12 +0200

    prod

--- manage.py
+++ manage.py
@@ -3,10 +3,13 @@
 import os
 import sys
 
+from ndh.utils import get_env
+
 
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testproject.settings')
+    get_env()
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
