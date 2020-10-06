c80880c63f597214525448643ae56da412922939
Guilhem Saurel <guilhem.saurel@laas.fr>
2020-10-06 12:57:41 +0200

    PoC

--- mergics/models.py
+++ mergics/models.py
@@ -1,3 +1,54 @@
-# from django.db import models
+from django.conf import settings
+from django.db import models
 
-# Create your models here.
+import requests
+from icalendar import Calendar
+
+from autoslug import AutoSlugField  # type: ignore
+from ndh.models import Links
+
+
+class ICSInput(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    url = models.URLField()
+
+    class Meta:
+        unique_together = ['user', 'url']
+
+    def __str__(self) -> str:
+        """Show the url of this input."""
+        return self.url
+
+    def from_ical(self) -> Calendar:
+        """Downloads and parse url."""
+        content = requests.get(self.url).content.decode()
+        return Calendar.from_ical(content)
+
+
+class ICSOutput(Links, models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    slug = AutoSlugField(populate_from='name')
+    inputs = models.ManyToManyField(ICSInput)
+
+    class Meta:
+        unique_together = ['user', 'slug']
+
+    def __str__(self) -> str:
+        """Get the name of the instance."""
+        return self.name
+
+    def to_cal(self) -> Calendar:
+        """Create a Calendar merging the inputs."""
+        cal = Calendar()
+        cal.add('prodid', f'-//{self.user} - {self.name}//{self.slug}.{self.user.username}.mergics//')
+        cal.add('version', '2.0')
+        for calendar in self.inputs.all():
+            for component in calendar.from_ical().walk():
+                if component.name != 'VCALENDAR':
+                    cal.add_component(component)
+        return cal
+
+    def to_ical(self) -> str:
+        """Format the generated Calendar as a string."""
+        return self.to_cal().to_ical()
--- mergics/tests.py
+++ mergics/tests.py
@@ -1,6 +1,30 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
+
+from . import models
+
+URLS = [
+    'https://www.mozilla.org/media/caldata/FrenchHolidays.ics',
+    'http://cache.media.education.gouv.fr/ics/Calendrier_Scolaire_Zone_C.ics'
+]
 
 
 class MergICSTests(TestCase):
     def test_nothing(self):
         self.assertTrue(True)
+
+    def test_models(self):
+        user = User.objects.create_user(username='test', password='test')
+        out = models.ICSOutput(user=user, name='holidays')
+        out.save()
+
+        components = 0
+
+        for url in URLS:
+            ics_input = models.ICSInput(user=user, url=url)
+            ics_input.save()
+            out.inputs.add(ics_input)
+            components += len(list(c for c in ics_input.from_ical().walk() if c.name != 'VCALENDAR'))
+
+        self.assertEqual(models.ICSOutput.objects.first().inputs.count(), 2)
+        self.assertEqual(len(models.ICSOutput.objects.first().to_cal().walk()) - 1, components)
