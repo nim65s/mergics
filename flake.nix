860a0df21959904fab3d2d09a6ae89118aca16db
Guilhem Saurel <guilhem.saurel@laas.fr>
2024-02-22 20:12:44 +0100

    use flake

---
+++ flake.nix
@@ -0,0 +1,57 @@
+{
+  description = "Application packaged using poetry2nix";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    poetry2nix = {
+      url = "github:nix-community/poetry2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      poetry2nix,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        # see https://github.com/nix-community/poetry2nix/tree/master#api for more functions and examples.
+        pkgs = nixpkgs.legacyPackages.${system};
+        inherit (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; }) mkPoetryApplication defaultPoetryOverrides;
+      in
+      {
+        packages = {
+          mergics = mkPoetryApplication {
+            projectDir = self;
+            overrides = defaultPoetryOverrides.extend (
+              self: super: {
+                django-autoslug = super.django-autoslug.overridePythonAttrs (
+                  old: { buildInputs = (old.buildInputs or [ ]) ++ [ super.setuptools ]; }
+                );
+                django-bootstrap5 = super.django-bootstrap5.overridePythonAttrs (
+                  old: { buildInputs = (old.buildInputs or [ ]) ++ [ super.setuptools ]; }
+                );
+                ndh = super.ndh.overridePythonAttrs (
+                  old: { buildInputs = (old.buildInputs or [ ]) ++ [ super.poetry-core ]; }
+                );
+                yeouia = super.yeouia.overridePythonAttrs (
+                  old: { buildInputs = (old.buildInputs or [ ]) ++ [ super.poetry-core ]; }
+                );
+              }
+            );
+          };
+          default = self.packages.${system}.mergics;
+        };
+
+        devShells.default = pkgs.mkShell {
+          inputsFrom = [ self.packages.${system}.mergics ];
+          packages = [ pkgs.poetry ];
+        };
+      }
+    );
+}
